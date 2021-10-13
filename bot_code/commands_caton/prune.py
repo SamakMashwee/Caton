bbf28b22b927b5a967e138a74f1c8b1f7f6a6f4a
Amazinggamer05 <amazinggamer05@hotmail.com>
2021-10-13 17:16:09 -0500

    prune command done

--- bot_code/commands_caton/prune.py
+++ bot_code/commands_caton/prune.py
@@ -1,5 +1,4 @@
-import math
-import discord
-
 async def execute(client, message, args, command, prefix):
-    await client.delete_messages([message for message in client.logs_from(message.channel, limit=int(args))])
+    #ADD PERMISSION#
+    de = int(args[0])
+    await message.channel.purge(limit=de + 1, bulk = True)
