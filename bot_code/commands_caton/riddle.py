62cbcc72f009c279a47f63fb4ab4f035903c93e0
SamakMashwee <samakmashwee@gmail.com>
2021-10-13 18:01:30 -0500

    added riddle command

--- bot_code/commands_caton/riddle.py
+++ bot_code/commands_caton/riddle.py
@@ -1,8 +1,18 @@
+from random import randint
 
-riddles_answers = {
-	0 : ["What's red and black and white all over?", "A monoracial sacrificial orgy."],
-	1 : ["What is green and fuzzy and will kill you if it falls out of a tree?","pool table."],
-  2: ["What is Paul's favorite word?", "Cummies in my tummies."]
-  
+riddles = []
 
-}
+with open("commands_caton/variables/riddles_and_answers.txt") as file:
+	data = file.readlines()
+	file.close()
+
+for counter in range(len(data)):
+	if counter % 2 == 0:
+		riddles.append([data[counter]])
+		riddles[int(counter/2)].append(data[counter+1])
+
+riddle_count = len(riddles)
+
+async def execute(client, message, args, command, prefix):
+	riddle_index = randint(0, riddle_count-1)
+	await message.channel.send(f"{riddles[riddle_index][0]}\n||{riddles[riddle_index][1]}||")
