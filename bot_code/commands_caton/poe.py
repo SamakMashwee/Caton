946f75f73b0664f3874246e492562abdd20d2733
SamakMashwee <samakmashwee@gmail.com>
2021-10-13 18:04:45 -0500

    removed print statement

--- bot_code/commands_caton/poe.py
+++ bot_code/commands_caton/poe.py
@@ -6,5 +6,3 @@ with open("commands_caton/variables/the_raven.txt", encoding="utf8") as file:
 
 async def execute(client, message, args, command, prefix):
     await message.channel.send(f"```{verses[randint(0, len(verses)-1)]}```")
-
-print(verses)
